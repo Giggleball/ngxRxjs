e7ef69f4b8f42f278daf53751f4ebf513278f8ae
Elisabeth Boldewijn <elisabeth.boldewijn@sns.nl>
2022-07-21 14:53:26 +0200

    Add share replay to prevent duplicate hppt calls from being made

--- src/app/services/courses.service.ts
+++ src/app/services/courses.service.ts
@@ -2,7 +2,7 @@ import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
 
-import {map} from 'rxjs/operators';
+import {map, shareReplay} from 'rxjs/operators';
 import {Course} from '../model/course';
 
 @Injectable({
@@ -19,7 +19,8 @@ export class CoursesService {
   loadAllCourses(): Observable<Course[]> {
     return this.http.get<Course[]>("/api/courses")
       .pipe(
-        map(res => res["payload"])
+        map(res => res["payload"]),
+        shareReplay()
       );
   }
 }
