c0befdb661976298c0f30aded2cbca8b74717f64
Elisabeth Boldewijn <elisabeth.boldewijn@sns.nl>
2022-07-21 15:20:25 +0200

    removed presentational logic from home component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -34,6 +34,7 @@ import {MessagesComponent} from './messages/messages.component';
 import {SearchLessonsComponent} from './search-lessons/search-lessons.component';
 import { LoadingComponent } from './loading/loading.component';
 import {CoursesService} from './services/courses.service';
+import { CoursesCardListComponent } from './courses-card-list/courses-card-list.component';
 
 @NgModule({
   declarations: [
@@ -47,7 +48,8 @@ import {CoursesService} from './services/courses.service';
     SafeUrlPipe,
     MessagesComponent,
     SearchLessonsComponent,
-    LoadingComponent
+    LoadingComponent,
+    CoursesCardListComponent
 
   ],
   imports: [
